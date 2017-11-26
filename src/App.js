0c63bfaf952f8ef1e4a97b27f0ca0c3c06a60758
Julian Schäfer <schaefer.julian@mail.de>
2017-11-26 22:36:36 +0100

    Using className isntead of class for JSX components

--- src/App.js
+++ src/App.js
@@ -2,32 +2,37 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-// import ControlPanel from '@components/control-panel'
-
 class App extends Component {
+
   render() {
     return (
       <div className="App">
         <h1>Graph Algorithm Simulator</h1>
-        <div class="overview flex-container">
-          {/* <ControlPanel class="overview-item" /> */}
-          <div class="overview-item flex-item--full"> 
-            <h2>ControlPanel</h2>
+        <div className="overview flex-container">
+          <div className="overview-item flex-item--full">
+            <h2>Control Panel</h2>
+            <div>
+              <select>
+                <option>DFS</option>
+                <option>BFS</option>
+              </select>
+              <input type="button" value="next" />
+            </div>
           </div>
-          <div class="overview-item flex-item--half">
+          <div className="overview-item flex-item--half">
             <h2>Algorithm</h2>
-            <div class="code-block">
+            <div className="code-block">
               <code>public static void main()</code>
-              <code class="debug-pointer">  int i = 43;</code>
+              <code className="debug-pointer">  int i = 43;</code>
               <code>  return i;</code>
               <code></code>
             </div>
           </div>
-          <div class="overview-item flex-item--half">
+          <div className="overview-item flex-item--half">
             <h2>Graph</h2>
             <div id="graph"></div>
           </div>
-          <div class="overview-item flex-item--full">
+          <div className="overview-item flex-item--full">
             <h2>Logs</h2>
             <ol>
               <li>Log entry ...</li>
@@ -40,5 +45,6 @@ class App extends Component {
     );
   }
 }
+
 
 export default App;
