a652e8a69bb9b212ef8c44fa35ac37e5246ec3e6
Julian Schäfer <schaefer.julian@mail.de>
2017-11-28 22:42:31 +0100

    Added immutable js and extending debugging of algorithms

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import { Map, Set, List } from 'immutable';
+
 class App extends Component {
 
   constructor(props) {
@@ -17,7 +19,7 @@ class App extends Component {
   }
 
   handleChange(event) {
-    this.setState({selectedAlgorithm: event.target.value});
+    this.setState({ selectedAlgorithm: event.target.value });
   }
 
   handleSubmit(event) {
@@ -37,25 +39,33 @@ class App extends Component {
     this.setState((prevState, props) => {
       const logs = executeAlgorithm("bfs", graph, 'a');
       return ({
-        logs: []
-      }); 
+        logs: logs,
+        currentLogLine: logs[0]
+      });
     });
 
     event.preventDefault();
   }
-  
+
   next() {
-    this.setState((prevState, props) => ({
-      selectedAlgorithm: prevState.selectedAlgorithm,
-      currentDebugLine: prevState.currentDebugLine + 1
-    }));
+    if (this.state.currentLogLine && (this.state.currentLogLine.id + 1) < this.state.logs.length) {
+      const nextLogLine = this.state.logs[this.state.currentLogLine.id + 1];
+      console.log("next: " + JSON.stringify(nextLogLine));
+      this.setState((prevState, props) => ({
+          currentLogLine: nextLogLine
+      }));
+    }
   }
 
   before() {
-    this.setState((prevState, props) => ({
-      selectedAlgorithm: prevState.selectedAlgorithm,
-      currentDebugLine: prevState.logs[currentLogLine + 1] - 1
-    }));
+    if (this.state.currentLogLine && this.state.currentLogLine.id > 0) {
+      const nextLogLineNumber = this.state.currentLogLine.id - 1;
+      const nextLogLine = this.state.logs[nextLogLineNumber];
+      console.log("before:" + JSON.stringify(nextLogLine));
+      this.setState((prevState, props) => ({
+          currentLogLine: nextLogLine
+      }));
+    }
   }
 
   render() {
@@ -63,18 +73,21 @@ class App extends Component {
     const algorithm = data.algorithms[0];
     const codeLines = algorithm.code.map((codeLine) => {
       var codeLineStyle = 'code-block-line';
-      if (codeLine.number === this.state.currentDebugLine)  {
+      if (this.state.currentLogLine && codeLine.number === this.state.currentLogLine.line) {
         codeLineStyle += ' debug-pointer';
       }
       return (
         <div key={codeLine.number.toString()} className={codeLineStyle}>
           <div className="line-number">{codeLine.number}.</div>
-          <div className="line-content" style={{ paddingLeft: `${codeLine.padding}em`}}>
+          <div className="line-content" style={{ paddingLeft: `${codeLine.padding}em` }}>
             {codeLine.content}
           </div>
         </div>
       );
     });
+
+    const renderedAlgorithm = renderAlgorithm(codeLines);
+
     return (
       <div className="App">
         <h1>Graph Algorithm Simulator</h1>
@@ -82,7 +95,7 @@ class App extends Component {
           <div className="overview-item flex-item--full">
             <h2>Control Panel</h2>
             <div>
-              <select value={this.state.selectedAlgorithm} onChange={this.handleChange}>  
+              <select value={this.state.selectedAlgorithm} onChange={this.handleChange}>
                 <option value="default">Choose</option>
                 <option value="dfs">DFS</option>
                 <option value="bfs">BFS</option>
@@ -93,10 +106,7 @@ class App extends Component {
             </div>
           </div>
           <div className="overview-item flex-item--half">
-            <h2>Algorithm</h2>
-            <div className="code-block">
-              {codeLines}
-            </div>
+            {renderedAlgorithm}
           </div>
           <div className="overview-item flex-item--half">
             <h2>Graph</h2>
@@ -117,7 +127,7 @@ function Data() {
   return {
     algorithms: [
       {
-        name: 'BFS', 
+        name: 'BFS',
         code: [
           { number: 1, padding: 0, content: 'Push(Q, s);' },
           { number: 2, padding: 0, content: 'Markiere s;' },
@@ -148,6 +158,17 @@ function Data() {
   }
 }
 
+
+const renderAlgorithm = codeLines => {
+  return (<div>
+      <h2>Algorithm</h2>
+      <div className="code-block">
+        {codeLines}
+      </div>
+    </div> 
+  );
+};
+
 function executeAlgorithm(algorithm, graph, startNode) {
   var logs = [];
   if (algorithm === 'bfs') {
@@ -159,49 +180,49 @@ function executeAlgorithm(algorithm, graph, startNode) {
 
 function bfs(nodes, edges, startNode) {
   var logs = [];
-  var d = {};
-  var Q = [];
-
-  logs.push({
-    id: 0,
-    line: 1,
-    state: `Q = ${Q}, d = ${d}`
-  });
-
-  d[startNode] = 0;
-  Q = [startNode];
-
-  logs.push({
-    id: logs[logs.length -1].id + 1,
-    state: `Q = ${Q}, d = ${d}`,
-    line: 6
-  });
-
-  while (Q.length !== 0) {
-    var currentNode = Q[0];
-    if (edges[currentNode].length !== 0) {
-      var nextNode = edges[currentNode][0];
-      if (d[nextNode] === undefined) {
-        d[nextNode] = d[currentNode] + 1;
-        Q.push(nextNode);
+  var d = Map();
+  var Q = List();
+  var N = Map();
+  extendLogs(1, bfsLogLine(Q, d, N), logs);  
+  Q = Q.push(startNode);
+  extendLogs(2, bfsLogLine(Q, d, N), logs);  
+  d = d.set(startNode, 0);
+  N = N.set(startNode, List.of(edges[startNode]));
+  extendLogs(6, bfsLogLine(Q, d, N), logs);
+  while (!Q.isEmpty()) {
+    var currentNode = Q.first();
+    if (!N.get(currentNode).isEmpty()) {
+      var nextNode = N.get(currentNode).first();
+      if (d.get(nextNode) === undefined) {
+        d = d.set(nextNode, d.get(currentNode) + 1);
+        N = N.set(nextNode, List.of(edges[nextNode]));
+        Q = Q.push(nextNode);
       } else {
-        edges[currentNode].splice(0,1);
+        var next = N.get(currentNode).delete(0);
+        N = N.set(currentNode, next);
       }
     } else {
-      Q.splice(0, 1);
+      Q = Q.pop();
     }
-
-    logs.push({
-      id: logs[logs.length -1].id + 1,
-      line: 22,
-      state: `Q = ${Q}, d = ${d}`
-    });
-
+    extendLogs(22, bfsLogLine(Q, d, N), logs);
   }
-
+  extendLogs(23, bfsLogLine(Q, d, N), logs);
   return logs;
 }
 
+function bfsLogLine(Q, d, N) {
+  return `Q = ${JSON.stringify(Q)}, d = ${JSON.stringify(d)}, N = ${JSON.stringify(N)}`;
+}
+
+function extendLogs(line, state, logs) {
+  const logId = logs.length; 
+  logs.push({
+    id: logId,
+    state: state,
+    line: line
+  });
+
+}
 
 
 export default App;
